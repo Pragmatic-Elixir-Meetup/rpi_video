576974762c5ba7ed51de09fe31a52f69f1b2fc5e
Steven Gu <steven.gu@letote.cn>
2019-10-08 21:41:10 +0800

    Implements the Elixir part of first version.

--- lib/rpi_video.ex
+++ lib/rpi_video.ex
@@ -1,2 +1,6 @@
 defmodule RpiVideo do
+  def start(mock \\ false), do: impl_mod(mock).start
+
+  defp impl_mod(false), do: RpiVideo.RealServer
+  defp impl_mod(true), do: RpiVideo.MockServer
 end
--- src/real_rpi_video.rs
+++ src/real_rpi_video.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, real_rpi_video!");
 }
